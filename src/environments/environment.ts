7c130081f818ca3d5dd3ec0167b7d70abe078bd4
Francesco Menghi <53121061+menghif@users.noreply.github.com>
2022-11-12 13:47:15 -0500

    Migrate API from Heroku to Render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   clientID: '63c592f6526e46539bd357b0a90627fa',
   clientSecret: '1e4ee6b8451b4af0a534aad8fb5915dc',
-  userAPIBase: 'https://seneca-music-users.herokuapp.com/api/users',
+  userAPIBase: 'https://seneca-music.onrender.com/api/users',
 };
 
 /*
